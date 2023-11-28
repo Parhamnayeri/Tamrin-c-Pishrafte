02a61701b15a5e46551c707ada0c9d1dc1681eb4
Parham nayeri <parham.ny.shiraz@gmail.com>
2023-11-29 01:04:56 +0330

    s

---
+++ DanshesgaSQL/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ClassReserv](
+    [Id] INT NOT NULL,
+    [Class-number] INT NULL, 
+    [Ostad-Id] INT NULL, 
+    [Dars-ID] INT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
